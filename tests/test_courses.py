6c1525fef2a2cf6b565a55fdcab9f132141f5ced
s-alessio <alessionaharia@gmail.com>
2025-06-30 11:37:26 +0300

    Практикуемся в реализации PageObject.  Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,7 @@
 import pytest
 from playwright.sync_api import expect
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.courses
 @pytest.mark.regression
@@ -22,3 +24,25 @@ def test_empty_courses_list(chromium_page_with_state):
     courses_empty_list_desc_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
     expect(courses_empty_list_desc_text).to_be_visible()
     expect(courses_empty_list_desc_text).to_have_text("Results from the load test pipeline will be displayed here")
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(courses_list_page:CoursesListPage, create_course_page:CreateCoursePage):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form("","","","0","0")
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form("Playwright","2 weeks","Playwright","100","10")
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(0,"Playwright","100","10","2 weeks")
+
+
