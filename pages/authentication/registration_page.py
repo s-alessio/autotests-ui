b62cf8d8a75f21a742aede01ed611e80b80c9a42
s-alessio <alessionaharia@gmail.com>
2025-07-01 15:54:58 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -5,6 +5,8 @@ from elements.button import Button
 from elements.link import Link
 from pages.base_page import BasePage
 
+import re
+
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
@@ -19,5 +21,7 @@ class RegistrationPage(BasePage):
     def click_registration_button(self):
         self.registration_button.click()
 
+
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
