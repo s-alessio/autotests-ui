fcf6209d163ecd55216981950bb50a197e18b1fd
s-alessio <alessionaharia@gmail.com>
2025-07-05 18:33:14 +0300

    Добавляем информацию о переменных окружения в Allure отчет

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath
     browser_state_file: FilePath
 
     def get_base_url(self) -> str:
@@ -47,11 +48,13 @@ class Settings(BaseSettings):
         # Указываем пути
         videos_dir = DirectoryPath("./videos")
         tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser-state.json")
 
         # Создаем директории, если они не существуют
         videos_dir.mkdir(exist_ok=True)  # Если директория сещуствует, то игнорируем ошибку
         tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)
         # Создаем файл состояния браузера, если его нет
         browser_state_file.touch(exist_ok=True)  # Если файл сещуствует, то игнорируем ошибку
 
@@ -59,6 +62,7 @@ class Settings(BaseSettings):
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
+            allure_results_dir=allure_results_dir,
             browser_state_file=browser_state_file
         )
 
--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 pytest_plugins = (
     "fixtures.pages", # Подключаем фикстуры страниц
+    "fixtures.allure",
     "fixtures.browsers" # Подключали ранее в предыдущих уроках
 )
